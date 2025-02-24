17e9cd47532a425a295fce3743b4c48c47aa766e
Kimsungbin1 <ksb19558@naver.com>
2025-02-25 01:29:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2662,7 +2662,7 @@ app.use(
     origin: [
       "https://attendhs-admin.netlify.app",
       "https://attendhs.netlify.app",
-      "http://localhost:5500",
+      "http://127.0.0.1:5500",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
